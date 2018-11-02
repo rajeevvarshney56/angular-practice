7c5b0f5c08a06f90be318e057432f4bbd9868313
rajeev <rajeev.varshney@sugaldamani.com>
2018-11-02 15:17:14 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{type:'server',serverName:'Testserver',serverContent:'just a server!'}];
+  serverElements = [{ type: 'server', serverName: 'Testserver', serverContent: 'just a server!' }];
+
+  onServerAdded(serverData:{serverName:string,serverContent:string}) {
+    this.serverElements.push({
+        type: 'server',
+        serverName: serverData.serverName,
+        serverContent: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData:{serverName:string,serverContent:string}) {
+    this.serverElements.push({
+        type: 'blueprint',
+        serverName: blueprintData.serverName,
+        serverContent: blueprintData.serverContent
+    });
+  }
+
+  OnChangeFirst(){
+    this.serverElements[0].serverName = 'Changed';
+  }
+
+  OnDestroyFirst(){
+    this.serverElements.splice(0,1);
+  }
 }
